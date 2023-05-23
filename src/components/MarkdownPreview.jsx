bbf53a0fec0adb57362e846a733ab3e442a03a81
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-05-23 22:02:07 +0200

    working markdown previewer

--- src/components/MarkdownPreview.jsx
+++ src/components/MarkdownPreview.jsx
@@ -1,16 +1,17 @@
 // import markdown library
 import { marked } from "marked";
+import DOMPurify from "dompurify";
 
 marked.use({
   headerIds: false,
   mangle: false,
+  breaks: true
 });
 
 const MarkdownPreview = ({ markdown }) => {
-  // const renderedHTML
   const render = marked.parse(markdown)
 
-  return <div dangerouslySetInnerHTML={{__html: render}} id="preview"></div> 
+  return <div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(render) }}></div>
 }
 
 export default MarkdownPreview;
