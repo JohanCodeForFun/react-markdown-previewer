fa22710bd2943b97c32d9e9fad988de9307438e6
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-05-23 21:03:53 +0200

    setup markdown component

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useId, useState } from 'react';
+import MarkdownPreview from './components/MarkdownPreview';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const id = useId();
+  const [input, setInput] = useState('');
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>Enter your markdown:</h1>
+     <textarea value={input} onInput={e => setInput(e.target.value)} name="editor" id="editor" cols="30" rows="10"></textarea>
+     {/* <div id='preview'></div> */}
+     <MarkdownPreview  markdown={input}/>
     </>
   )
 }
---
+++ src/components/MarkdownPreview.jsx
@@ -0,0 +1,8 @@
+// import markdown library
+
+const MarkdownPreview = ({ markdown }) => {
+  // const renderedHTML...
+  return <div id="preview">{ markdown }</div> 
+}
+
+export default MarkdownPreview;
