c20e83d303d28afae1f0128e6190651f2e01c4fe
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-05-23 21:30:07 +0200

    working markdown

--- src/components/MarkdownPreview.jsx
+++ src/components/MarkdownPreview.jsx
@@ -1,8 +1,16 @@
 // import markdown library
+import { marked } from "marked";
+
+marked.use({
+  headerIds: false,
+  mangle: false,
+});
 
 const MarkdownPreview = ({ markdown }) => {
-  // const renderedHTML...
-  return <div id="preview">{ markdown }</div> 
+  // const renderedHTML
+  const render = marked.parse(markdown)
+
+  return <div dangerouslySetInnerHTML={{__html: render}} id="preview"></div> 
 }
 
 export default MarkdownPreview;
