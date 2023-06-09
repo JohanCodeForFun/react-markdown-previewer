0dd095f64e1014eb1136f7e77a07246033043a26
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-06-09 21:59:40 +0200

    remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import './App.css'
-import { useEffect, useId, useState } from 'react';
+import { useEffect, useState } from 'react';
 import MarkdownPreview from './components/MarkdownPreview';
 import presetMarkdown from './data/presetMarkdown';
 
 function App() {
-  // const id = useId();
   const [input, setInput] = useState('');
 
   useEffect(() => {
@@ -15,7 +14,6 @@ function App() {
     <>
      <h1>Enter your markdown:</h1>
      <textarea value={input} onInput={e => setInput(e.target.value)} name="editor" id="editor" cols="30" rows="10"></textarea>
-     {/* <div id='preview'></div> */}
      <MarkdownPreview  markdown={input}/>
     </>
   )
